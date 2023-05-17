7d3725b91510fcf6734f7d41f9c5e1c83b0b4e6f
Caty Ramy <catherineramy02@gmail.com>
2023-05-18 01:11:37 +0300

    adding basic files for lang ar and translate them & the dropdown menu for lang

--- laravel_app/resources/views/Pages/Header.blade.php
+++ laravel_app/resources/views/Pages/Header.blade.php
@@ -4,17 +4,39 @@
 <head>
     <title>Header Page </title>
     <link href="/CSS/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
 </head>
 
 <body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <header>
         <nav>
             <div class="logo">
                 <img src="/IMG/logo.png" alt="Logo">
             </div>
+            <div>
+
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-toggle="dropdown" aria-expanded="false">
+                        Change Language
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">English</a></li>
+                        <li><a class="dropdown-item" href="#">Arabic</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
             <ul>
             <li><a href="{{ url('/') }}">Home Page</a></li>
             <li><a href="{{ url('/register') }}">Register</a></li>
+
+
+
 
             </ul>
         </nav>
